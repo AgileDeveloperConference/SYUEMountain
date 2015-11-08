bf1d577d67439c896a6c6eb8d57fd502867a557c
tonyk <jinniw43805@gmail.com>
2015-11-08 23:44:24 +0800

    add: donate save api

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 
 module.exports = {
-	key:"AtKzFOGMGcypAKAUotgBPiiicsGd_kTQDCbpxdkscWtbaYrNR3EIJy5I-g8fPfU0"
+	key: "AtKzFOGMGcypAKAUotgBPiiicsGd_kTQDCbpxdkscWtbaYrNR3EIJy5I-g8fPfU0",
+	dbURL: "mongodb://tony:tony123@ds045464.mongolab.com:45464/agilemount"
 }
---
+++ src/model/donateModel.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
+var config = require('../../config');
+
+var Schema = mongoose.Schema;
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
+var mongodbUri = config.dbURL;
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+
+if(mongoose.connection.readyState == 0){
+  mongoose.connect(mongooseUri, options);
+}
+module.exports = mongoose.model('DonateHistory',new Schema({
+		fbUID : String,
+		date : Number,
+		charityID : Number,
+		contributeValue:Number
+}));
